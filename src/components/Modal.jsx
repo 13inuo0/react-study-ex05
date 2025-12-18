e80fe70bc73b961a9bd394e8e7faed466f164676
13inuo0 <13inuo0@github.com>
2025-12-18 10:55:35 +0900

    모달창 버튼 누르면 모달창 띄우기

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,34 @@
-import { ModalEx } from "./ModalEx";
+import { useEffect } from "react";
+import "./Modal.css";
 
-export function Modal(){
-
+export function Modal({ isOpen, title }) {
+  useEffect(() => {
+    if (isOpen) {
+      // 현재 스크롤 위치 저장
+      const scrollY = window.scrollY;
+      //   body에 스크롤 방지 스타일 적용
+      document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = "100%";
+      //   컴포넌트가 언마운트될 때 스타일 복원
+      return () => {
+        document.body.style.overflow = "";
+        document.body.style.position = "";
+        document.body.style.top = "";
+        document.body.style.width = "";
+        // 스크롤 위치 복원
+        window.scrollTo(0, scrollY);
+      };
+    }
+  }, [isOpen]);
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h2 className="modal-title">{title}</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/ModalEx.jsx
+++ src/components/ModalEx.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import "./ModalEx.css";
+import { Modal } from "./Modal";
 
 export function ModalEx() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
   return (
     <div className="modal-example">
       <h1>모달 컴포넌트 사용 예제</h1>
       <div className="button-group">
-        <button className="btn btn-primary">기본 모달열기</button>
+        <button className="btn btn-primary" onClick={openModal}>
+          기본 모달열기
+        </button>
         <button className="btn btn-warning">확인 모달열기</button>
         <button className="btn btn-success">폼 모달열기</button>
       </div>
+      {/* 기본 모달 */}
+      <Modal isOpen={isModalOpen} title="기본 모달">
+        <div className="modal-content-example">
+            <p>이것은 기본 모달입니다.</p>
+        </div>
+      </Modal>
     </div>
   );
 }
